3a7798b4abad3d2542a877c797dd20e235eb14c4
JohnBeck19 <jbeck@student.neumont.edu>
2025-03-02 21:31:28 -0700

    ads.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2114112298508670"crossOrigin="anonymous"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
